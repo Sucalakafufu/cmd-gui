fe70b04c3f0912c9f7c634716f7e2a595035e3e5
sucalakafufu@gmail.com <sucalakafufu@gmail.com@1fbdcb7c-e3cb-11de-87fd-f59fab27b0b2>
2009-12-14 11:35:54 +0000

    now linked to CMD Prompt

--- trunk/CMD_GUI.ui.h
+++ trunk/CMD_GUI.ui.h
@@ -8,14 +8,23 @@
 *****************************************************************************/
 #include <qstatusbar.h>
 #include <windows.h>
+#include <iostream>
+//#include <fcntl.h>
+//#include <io.h>
+#include <string>
 
 void CMD_GUI::init() //called in constructor
 {
 	settings = new Settings(this,0,true,0); //create new settings dialog and about dialog 
 	about = new About(this,0,true,0);
+	FreeConsole(); AllocConsole();
+	AttachConsole(GetCurrentProcessId());
+	/*int inHandle = _open_osfhandle((long)GetStdHandle(STD_INPUT_HANDLE), _O_TEXT );
+	FILE *inOpenHandle = _fdopen( inHandle, "r" );
+	*stdin = *inOpenHandle;*/
 }
 
-void CMD_GUI::showSettings() //set fixed size and show settings dialog
+void CMD_GUI::showSettings() //show settings dialog
 {
 	settings->show();
 }
@@ -25,7 +34,7 @@ void CMD_GUI::openCMD() //start CMD Prompt
 	system("START CMD");
 }
 
-void CMD_GUI::showAbout() //set fixed size and show about dialog
+void CMD_GUI::showAbout() //show about dialog
 {
 	about->show();
 }
@@ -81,9 +90,14 @@ void CMD_GUI::cmdStart()
 
 void CMD_GUI::sendCommand()
 {
-	system(CMD_lineEdit->text().latin1()); //figure out how to send this and keep a command line open
+	if (!CMD_lineEdit->text().isEmpty())
+		system(CMD_lineEdit->text().latin1()); //figure out how to send this and keep a command line open
+	std::string test;
+	/*while ()
+	{
+		commands->setText(tr(test.c_str()));
+	}*/
 	//TODO: Have lineEdit go to cmd
-	//AttachConsole(ATTACH_PARENT_PROCESS(DWORD)-1);
 	//Remembers line in combo box, max size is 10.
 	lastTen_comboBox->setMaxCount(8);
 	lastTen_comboBox->insertItem(CMD_lineEdit->text(), 0);
